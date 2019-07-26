2a0638ef63ea9ee82b7f86fecbbad5b4bf91254e
NohaSamir <eng.nohasamirsaad@gmail.com>
2019-07-26 08:03:29 +0200

    commit

--- app/src/main/java/com/example/android/popularmovies/activity/MovieDetails.java
+++ app/src/main/java/com/example/android/popularmovies/activity/MovieDetails.java
@@ -2,15 +2,15 @@ package com.example.android.popularmovies.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.databinding.ActivityMovieDetailsBinding;
 import com.example.android.popularmovies.model.Movie;
 
 public class MovieDetails extends AppCompatActivity {
@@ -27,8 +27,9 @@ public class MovieDetails extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_details);
 
+        //ToDo 6: remove all old code
+       /* setContentView(R.layout.activity_movie_details);
         moviePoster = findViewById(R.id.moviePoster);
         movieImage = findViewById(R.id.movieImage);
         movieTitle = findViewById(R.id.moviewTitle);
@@ -48,8 +49,29 @@ public class MovieDetails extends AppCompatActivity {
             movieReleaseDate.setText(movie.getReleaseDate());
 
             String image_URL = IMAGE_BASE_URL + movie.getPosterPath();
+            Log.d("Image", image_URL);
             Glide.with(this).load(image_URL).into(moviePoster);
             Glide.with(this).load(image_URL).into(movieImage);
+        }*/
+
+        //ToDo 7: create instance from data binding using the name of xml
+        //activity_movie_details => Activity Movie Details Binding
+        ActivityMovieDetailsBinding detailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_movie_details);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            movie = intent.getParcelableExtra("Movie");
+
+            //ToDo 8 : pass the model to the defined variable inside  xml
+            detailsBinding.setMovie(movie);
         }
+    }
+
+
+    public static void start(Context context, Movie movie) {
+        Intent intent = new Intent(context, MovieDetails.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("Movie", movie);
+        context.startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android/popularmovies/adapter/MoviesAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/adapter/MoviesAdapter.java
@@ -1,18 +1,15 @@
 package com.example.android.popularmovies.adapter;
 
 import android.content.Context;
-import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.example.android.popularmovies.R;
 import com.example.android.popularmovies.activity.MovieDetails;
+import com.example.android.popularmovies.databinding.ListItemsBinding;
 import com.example.android.popularmovies.model.Movie;
 
 import java.util.List;
@@ -20,60 +17,91 @@ import java.util.List;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
 
-    private List<Movie> movies;
-    private int rowLayout;
-    private Context context;
-    public static final String IMAGE_URL_BASE = "https://image.tmdb.org/t/p/w185";
 
-    public static class MovieViewHolder extends RecyclerView.ViewHolder {
-        RelativeLayout moviesLayout;
+    private List<Movie> movies;
+    private Context context;
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        //ToDo 10: edit view holder to take ItemBinding instead of View
+
+        /*RelativeLayout moviesLayout;
         TextView movieTitle;
         ImageView movieImage;
 
-        public MovieViewHolder(View v) {
+        MovieViewHolder(View v) {
             super(v);
             moviesLayout = (RelativeLayout) v.findViewById(R.id.movies_layout);
             movieTitle = (TextView) v.findViewById(R.id.title);
             movieImage = (ImageView) v.findViewById(R.id.item_movie_poster);
+        }*/
+
+
+        private ListItemsBinding itemsBinding;
+
+        MovieViewHolder(ListItemsBinding listItemsBinding) {
+            super(listItemsBinding.getRoot());
+            this.itemsBinding = listItemsBinding;
         }
     }
 
-    public MoviesAdapter(List<Movie> movies, int rowLayout, Context context) {
+    public MoviesAdapter(Context context, List<Movie> movies) {
         this.movies = movies;
-        this.rowLayout = rowLayout;
         this.context = context;
     }
 
+    @NonNull
     @Override
-    public MoviesAdapter.MovieViewHolder onCreateViewHolder(ViewGroup parent,
-                                                            int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(rowLayout, parent, false);
-        return new MovieViewHolder(view);
+    public MoviesAdapter.MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        //ToDo 11 : use DataBindingUtil.inflate
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ListItemsBinding listItemsBinding = DataBindingUtil.inflate(layoutInflater, R.layout.list_items, parent, false);
+
+        //Delete this line
+        //View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_items, parent, false);
+        return new MovieViewHolder(listItemsBinding);
     }
 
     @Override
-    public void onBindViewHolder(MovieViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final MovieViewHolder holder, final int position) {
 
-        String image_url = IMAGE_URL_BASE + movies.get(position).getPosterPath();
 
+        //ToDo 12: bind item data
+        holder.itemsBinding.setMovie(movies.get(position));
+        /*
+         String image_url = IMAGE_URL_BASE + movies.get(position).getPosterPath();
         Glide.with(context).load(image_url).into(holder.movieImage);
-
         holder.movieTitle.setText(movies.get(position).getTitle());
-        holder.moviesLayout.setOnClickListener(new View.OnClickListener() {
+        */
+
+        //ToDo 14 : set my handler
+        holder.itemsBinding.setClickHandler(new ClickHandlers());
+
+      /*  holder.moviesLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, MovieDetails.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("Movie", movies.get(position));
+                intent.putExtra("Movie", movies.get(holder.getAdapterPosition()));
                 context.startActivity(intent);
             }
-        });
+        });*/
 
     }
 
     @Override
     public int getItemCount() {
         return movies.size();
+    }
+
+
+    //ToDo 13:  Define my click handler
+    public class ClickHandlers {
+        public void onClickMovie(Movie movie) {
+            MovieDetails.start(context, movie);
+        }
     }
 }
 
